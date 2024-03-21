ac32ec33a164f3640161e6c598195b544b4ee73d
Joshua Bryant <joshuabryantza@gmail.com>
2024-03-21 10:02:02 -0700

    Added the post and get api for category and product

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -2,7 +2,7 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
-class Category extends Model {}
+class Category extends Model { }
 
 Category.init(
   {
@@ -14,7 +14,7 @@ Category.init(
     },
     category_name: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: false
     },
   },
   {
--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -3,22 +3,52 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get('/', async (req, res) => {
+  try {
+    const categoryData = await Category.findAll();
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.findOne({ where: { id: req.params.id } });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const newCategory = await Category.create({
+      category_name: req.body.category_name
+    });
+
+    res.status(201).json(newCategory);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Failed to create category' });
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const updatedCategory = await Category.update(
+      { category_name: req.body.category_name },
+      { where: { id: req.params.id } }
+    );
+    res.json(updatedCategory);
+  }
+  catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Failed to find category' })
+
+  }
 });
 
 router.delete('/:id', (req, res) => {
